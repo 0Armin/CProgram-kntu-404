93c8c7888bd536245ee511ca53d168edbd796981
Armin <armin.najafpour@gmail.com>
2026-01-05 13:06:17 +0330

    small changes

--- Cell-Sheet/sheet.h
+++ Cell-Sheet/sheet.h
@@ -4,26 +4,30 @@
 #define Max_satr 100
 #define Max_soton 100
 #define Max_formula_tol 256
+
 typedef enum{
     //نوع خطاها
     Err_NONE,Err_taghsim_sefr,Err_tabe_nashenakhte_formul,Err_erjaa_namotabar_beselol,Err_parantez,Err_damane_tabe,Err_fayl,Err_ghaleb_dade
 }CellError;
+
 typedef struct{
     char address[10];
     //آدرس متنی سلول
     double value;
     //مقدار عدد ذخیره شده در سلول
-    char formula{Max_formula_tol};
+    char formula[Max_formula_tol];   //چه کردی؟ check later
     //رشته فرمول
     CellError error;
     //خطاها
 }Cell;
+
 typedef struct{
     int satr;
     int soton;
     Cell cells[Max_satr][Max_soton];
     //ماتریس دو بعدی برای سلول
 }Sheet;
+
 //توابع مربوط به ستون ها و آدرس ها
 int tabdil_esm_be_andisaddadi(const char* name);
 void andisaddadi_be_esm(int andisaddadi, char*buffer);
@@ -50,7 +54,7 @@ int saveSheetCSV(const Sheet* sheet,const char* filename);
 int loadSheetCSV(Sheet sheet,const char* filename);
 
 //خطاها
-void setCellError(Cell* cell,CellEROR err);
+void setCellError(Cell* cell,CellError err);
 //تنظیم وضعیت خطا برای یک سلول
 void clearCellError(Cell* cell);
 //پاک کردن خطا
