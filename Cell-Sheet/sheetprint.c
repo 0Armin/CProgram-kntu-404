38a236898e587deda331738bd2c5844091149cbb
alimaghamifar85-web <alimaghamifar85@gmail.com>
2026-01-20 16:34:52 +0330

    codenew2

--- Cell-Sheet/sheetprint.c
+++ Cell-Sheet/sheetprint.c
@@ -1,10 +1,12 @@
 #include "sheet.h"
 #include <stdio.h>
 void printSheet(const Sheet* sheet){
+    //این تابع کل جدول رو با جزییات ش چاپ میکنه
     if(!sheet){
         return;
         //وقتی اشاره گر جدول تهی بود خارج میشه
     }
+    //تو حلفه پایین روی تمام سلول های جدول حرکت میکنه
     for(int i=0; i<(*sheet).satr; i++){
         for(int j=0; j<(*sheet).soton; j++){
             const Cell* cell = &(*sheet).cells[i][j];
@@ -14,6 +16,7 @@ void printSheet(const Sheet* sheet){
     }
 }
 void printSheet_mahdod(const Sheet* sheet,int maxsatr,int maxsoton){
+    //این تابع یک بخش مشخصی از جدول رو چاپ میکنه
     if(!sheet){
         return;
         //وقتی اشاره گر جدول تهی بود خارج میشه
@@ -24,6 +27,7 @@ if( maxsatr > (*sheet).satr){
 if( maxsoton > (*sheet).soton){
     maxsoton = (*sheet).soton
 }
+//در حلقه پایین روی سطر و ستون ها حرکت میکنه
 for(int i=0; i<maxsatr; i++){
     for(int j=0; j<maxsoton; j++){
         const Cell* cell = &(*sheet).cells[i][j];
