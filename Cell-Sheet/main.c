243e71a93084ad487c27383707c89f0b19eb0d8a
alimaghamifar85-web <alimaghamifar85@gmail.com>
2026-02-02 13:13:42 +0330

    taghirat asli

--- Cell-Sheet/main.c
+++ Cell-Sheet/main.c
@@ -1,45 +1,109 @@
 #include "sheet.h"
 #include <stdio.h>
 int main() {
-    printf("=== شروع برنامه شبیه‌ساز جدول ===\n\n");
     Sheet sheet;
-    // 1) مقداردهی اولیه جدول
-    printf("ایجاد جدول 2x2 ...\n");
-    initSheet(&sheet, 2, 2);
-    printSheet(&sheet);
-    printf("\n--------------------------------------\n");
-    // 2) مقداردهی به چند سلول
-    printf("تنظیم مقدار برای A1 و B1 ...\n");
-    setCellValue(&sheet, "A1", 10.5);
-    setCellValue(&sheet, "B1", 20.25);
-    printf("تنظیم فرمول برای A2 ...\n");
-    setCellFormula(&sheet, "A2", "A1+B1");
-    printSheet(&sheet);
-    printf("\n--------------------------------------\n");
-    // 3) تست خطا
-    printf("تست خطا: تقسیم بر صفر در B2 ...\n");
-    Cell* c = getCell(&sheet, "B2");
-    if (c) {
-        setCellError(c, Err_taghsim_sefr);
+    int init = 0;
+    while (1) {
+        menu();
+        int choice;
+        scanf("%d", &choice);
+        if (choice == 1) {
+            int satr, soton;
+            printf("تعداد سطر: ");
+            scanf("%d", &satr);
+            printf("تعداد ستون: ");
+            scanf("%d", &soton);
+            initSheet(&sheet, satr, soton);
+            init = 1;  //نقش یک پرچم رو بازی میکنه که متوجه بشیم آیا جدول مقدار دهی انجام شده یا خیر که عدد 1 یعنی حدول آماده استفاده کردن شده است
+            printf("جدول مقداردهی شد.\n");
+        }
+        else if (choice == 2) {
+            if (!init) {
+                //بررسی اینکه جدول خالی نباشه و اول جدول ساخته شود
+                printf("اول جدول را مقداردهی اولیه کنید.\n");
+                continue;
+            }
+            char address[10];
+            double value;
+            printf("آدرس سلول (مثلاً A1): ");
+            scanf("%s", address);
+            printf("مقدار عددی: ");
+            scanf("%lf", &value);
+            if (setCellValue(&sheet, address, value))
+                printf("مقدار تنظیم شد.\n");
+            else
+                printf("خطا: آدرس نامعتبر.\n");
+        }
+        else if (choice == 3) {
+            if (!init) {
+                printf("اول جدول را مقداردهی اولیه کنید\n");
+                continue;
+            }
+            char address[10];
+            char formula[Max_formula_tol];
+            printf("آدرس سلول: ");
+            scanf("%s", address);
+            printf("فرمول: ");
+            scanf("%s", formula);
+            if (!sanitize_Formula(formula)) {
+                printf("فرمول نامعتبر است.\n");
+                continue;
+            }
+            if (setCellFormula(&sheet, address, formula))
+                printf("فرمول تنظیم شد.\n");
+            else
+                printf("خطا: آدرس نامعتبر.\n");
+        }
+        else if (choice == 4) {
+            if (!init) {
+                printf("اول جدول را مقداردهی اولیه کنید.\n");
+                continue;
+            }
+            printSheet(&sheet);
+        }
+        else if (choice == 5) {
+            if (!init) {
+                printf("اول جدول را مقداردهی اولیه کنید.\n");
+                continue;
+            }
+            int maxsatr, maxsoton;
+            printf("حداکثر سطر: ");
+            scanf("%d", &maxsatr);
+            printf("حداکثر ستون: ");
+            scanf("%d", &maxsoton);
+           printSheet_mahdod(&sheet, maxsatr, maxsoton);
+        }
+        else if (choice == 6) {
+            if (!init) {
+                printf("اول جدول را مقداردهی اولیه کنید.\n");
+                continue;
+            }
+            int satr_jadid, soton_jadid;
+            printf("سطر جدید: ");
+            scanf("%d", &satr_jadid);
+            printf("ستون جدید: ");
+            scanf("%d", &soton_jadid);
+            taghir_size_Sheet(&sheet, satr_jadid, soton_jadid);
+            printf("اندازه جدول تغییر کرد.\n");
+        }
+        else if (choice == 7) {
+            printf("خروج از برنامه...\n");
+            break;
+        }
+        else {
+            printf("گزینه نامعتبر.\n");
+        }
     }
-    printSheet(&sheet);
-    printf("\n--------------------------------------\n");
-    // 4) تغییر اندازه جدول
-    printf("تغییر اندازه جدول به 4x3 ...\n");
-    taghir_size_Sheet(&sheet, 4, 3);
-    printSheet(&sheet);
-    printf("\n--------------------------------------\n");
-    // 5) چاپ محدود
-    printf("چاپ محدود (2x2):\n");
-    printSheet_mahdod(&sheet, 2, 2);
-    printf("\n--------------------------------------\n");
-    // 6) تست آدرس نامعتبر
-    printf("تست آدرس نامعتبر: Z100 ...\n");
-    Cell* wrong = getCell(&sheet, "Z100");
-    if (!wrong) {
-        printf("آدرس Z100 نامعتبر است.\n");
-    }
-    printf("\n--------------------------------------\n");
-    printf("=== پایان برنامه ===\n");
     return 0;
+}
+void menu() {
+    printf("\n===== منوی اصلی =====\n");
+    printf("1) مقداردهی اولیه جدول\n");
+    printf("2) تنظیم مقدار یک سلول\n");
+    printf("3) تنظیم فرمول یک سلول\n");
+    printf("4) چاپ کامل جدول\n");
+    printf("5) چاپ محدود جدول\n");
+    printf("6) تغییر اندازه جدول\n");
+    printf("7) خروج\n");
+    printf("گزینه را وارد کنید: ");
 }
