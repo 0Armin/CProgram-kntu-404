60949b4eae58ee77bde2a83f07f8347e9f8a1fe2
ASheikhzadeh <amirhosseinsheikhzadeh3@gmail.com>
2026-02-04 23:17:41 +0000

    Update Formula.c

--- Formula/Formula.c
+++ Formula/Formula.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include "split_input.h"
 
-//void free_list(part_list list);//بعد از انجام محاسبه حافظه را آزاد میکند
-
-//double formula(char *input, Sheet *sheet){
-   // int err = ERR_NONE;
-    //part_list list = split(input);//تبدیل استرینگ به چیزی که میتوان با ان کار کرد
-
-    //double result = parse_and_calculate(list, &sheet,&err);//محاسبه
-
-    //free_list(list);//ازادسازی حافظه
-
-
-    
-  //  return result;//برگرداندن مقدار محاسبه شده
-//}
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
